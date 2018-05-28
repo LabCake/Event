0363c29f6211ddfc26f3f273c8f34e661820d304
Wuild <wuildz@gmail.com>
2018-05-28 20:11:16 +0200

    a

--- example.php
+++ example.php
@@ -1,14 +1,6 @@
 <?php
 
 /**
- * Example file on how to use the System library
+ * Example file on how to use the Event library
  */
 
-include_once "src/System.php";
-
-\LabCake\System::is_cli();
-\LabCake\System::is_https();
-\LabCake\System::is_windows();
-\LabCake\System::is_unix();
-
-\LabCake\System::get_os();
